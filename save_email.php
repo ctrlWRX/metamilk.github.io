842f23c4eb1a8223652eabd7e166dd2f03a6ef69
Boosted Subie <boosted@subiegang.com>
2026-01-04 18:09:08 -0600

    123

--- save_email.php
+++ save_email.php
@@ -33,10 +33,7 @@ try {
   }
   
   // Prepare SQL statement (prevents SQL injection)
-  $stmt = $conn->prepare(
-    "INSERT INTO user_profiles (username, first_name, last_name, birthday, email, timezone)"
-    . " VALUES (NULL, NULL, NULL, NULL, ?, NULL)"
-  );
+  $stmt = $conn->prepare("INSERT INTO user_profiles (email) VALUES (?)");
   
   if (!$stmt) {
     throw new Exception("Prepare failed: " . $conn->error);
